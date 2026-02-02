f66d63c0d19c34620a4b19d42b2b924426563788
Ainur Boranova <241970@astanait.edu.kz>
2026-02-03 00:28:10 +0500

    adding auction token

---
+++ contracts/AuctionToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AuctionToken is ERC20, Ownable {
+    constructor(address initialOwner)
+    ERC20("Auction Reward Token", "ART")
+    Ownable(initialOwner)
+    {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
